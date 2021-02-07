444e19c30630fa0274b2f258d52eb386228ac9e8
edel1212 <50935771+edel1212@users.noreply.github.com>
2021-02-07 18:05:16 +0900

    javscript
    javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,94 @@
+'use strict';
 
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll',() => {
+    if( window.scrollY > navbarHeight ){
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+});//event
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event) =>{
+    const tratget = event.target;
+    const link = tratget.dataset.link;
+    if( link === null ){
+        return;
+    }//if
+    navbarMenu.classList.remove('open');
+    scrollIntoView(link);
+
+});//event
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener( 'click', () => {
+    navbarMenu.classList.toggle('open');
+});//click
+
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener('click',()=>{
+    scrollIntoView("#contact");
+});//btn-click
+
+
+// Make home slowly fade to transparent as the window scolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+    home.style.opacity = 1- window.scrollY/homeHeight;
+});//scroll
+
+const arrowUp = document.querySelector('.arrow-up');
+//  SHow "arrow up" button when scolling down
+document.addEventListener("scroll",()=>{
+    if(window.scrollY > homeHeight/2){
+        arrowUp.classList.add('visible');
+    }else{        
+        arrowUp.classList.remove('visible');
+    }
+})//
+
+
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click',()=>{
+    scrollIntoView("#home");
+})
+
+// Projects
+const workBtnContainer = document.querySelector(".work__categories");
+const porjectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click',(e) =>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if( filter === null ){
+        return;
+    }//if
+    // Remvoe select from the pervoius item and select the Button
+    const active = document.querySelector(".category__btn.selected");
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('selected');
+    porjectContainer.classList.add('anim-out'); 
+    setTimeout(()=>{
+        projects.forEach((project) => {
+            if( filter === '*' || filter === project.dataset.type  ){
+                project.classList.remove('invisible');
+            } else{
+                project.classList.add('invisible');
+            }
+        });
+        porjectContainer.classList.remove('anim-out');
+   },300);//settimeout
+});//click
+
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior : 'smooth'});
+}//fun scollFunction
